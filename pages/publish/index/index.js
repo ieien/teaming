d30802ae40cb414fe2152bb8b1cfc93b8252c809
ieien <l.c.zw.258@gmail.com>
2019-11-19 21:41:22 +0800

    update

--- pages/publish/index/index.js
+++ pages/publish/index/index.js
@@ -1,60 +1,15 @@
 // pages/publish/index/index.js
+
+const app = getApp();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    proList: [
-      {
-        title: "昌航项目合作平台期待你的加入哦",
-        text1: "700",
-        text2: "600",
-        text3: "公示中"
-      },
-      {
-        title: "一个让你身心愉悦的小程序",
-        text1: "10",
-        text2: "0",
-        text3: "过期"
-      },
-      {
-        title: "早睡早起身体好，好好学习天天向上",
-        text1: "70",
-        text2: "30",
-        text3: "过期"
-      },
-      {
-        title: "早睡早起身体好，好好学习天天向上",
-        text1: "70",
-        text2: "30",
-        text3: "过期"
-      },
-      {
-        title: "早睡早起身体好，好好学习天天向上",
-        text1: "70",
-        text2: "30",
-        text3: "过期"
-      },
-      {
-        title: "早睡早起身体好，好好学习天天向上",
-        text1: "70",
-        text2: "30",
-        text3: "过期"
-      },
-      {
-        title: "早睡早起身体好，好好学习天天向上",
-        text1: "70",
-        text2: "30",
-        text3: "过期"
-      },
-      {
-        title: "早睡早起身体好，好好学习天天向上",
-        text1: "70",
-        text2: "30",
-        text3: "过期"
-      },
-    ],
+    //我发布的信息列表,[{news_id, title, views, likes, remain_time, post_time}, {}, ...]
+    myPublicList: [],    
+
     book_img: "/image/add_page/book.png",
     left_img: "/image/add_page/left.png",
     add_img: "/image/add_page/add.png",
@@ -65,6 +20,32 @@ Page({
       url: '/pages/publish/add/add',
     })
   },
+
+  /**
+   * 获得我发布的信息，得到一个对象数组
+   */
+  getMyPublicList: function(){
+    const _this = this;
+    wx.request({
+      url: 'https://teaming.malateam.cn/src/my_public_list.php',
+      data: {
+        user_id: app.globalData.openid
+      },
+      success(res) {
+        console.log(res);
+        if(res.data.code === 0){
+          _this.setData({
+            myPublicList: res.data.data
+          })
+        }else{
+          console.log('获取我的发布信息失败');
+        }
+      }
+    })
+  },
+
+
+
   
 
   /**
@@ -85,7 +66,7 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    this.getMyPublicList();
   },
 
   /**
